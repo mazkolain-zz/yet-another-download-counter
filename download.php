7672410e8abf68b397fa6d969c8801d45a6b50ca
Mikel Azkolain <azkotoki@gmail.com>
2013-03-09 10:18:48 +0100

    Added license stuff.

--- download.php
+++ download.php
@@ -1,5 +1,23 @@
 <?php
 
+	/*
+	 * This file is part of "Yet another download counter".
+	 * 
+	 * "Yet another download counter" is free software: you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as published by
+	 * the Free Software Foundation, either version 3 of the License, or
+	 * (at your option) any later version.
+	 * 
+	 * "Yet another download counter" is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 * GNU General Public License for more details.
+	 * 
+	 * You should have received a copy of the GNU General Public License
+	 * along with Foobar.  If not, see <http://www.gnu.org/licenses/>
+	 */
+
+
 	require(dirname(__FILE__).'/../../../wp-load.php');
 	
 	yadc_handle_downlad();
--- yet-another-download-counter.php
+++ yet-another-download-counter.php
@@ -5,10 +5,30 @@
 	Plugin URI: http://azkotoki.org
 	Description: Another download counter plugin. Simple, clean, no new tables.
 	Author: Mikel Azkolain
-	Version: 1.0
+	Version: 0.1
 	Author URI: http://azkotoki.org
+	License: GPL3
 	*/
 	
+
+	/*
+	 * This file is part of "Yet another download counter".
+	 * 
+	 * "Yet another download counter" is free software: you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as published by
+	 * the Free Software Foundation, either version 3 of the License, or
+	 * (at your option) any later version.
+	 * 
+	 * "Yet another download counter" is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 * GNU General Public License for more details.
+	 * 
+	 * You should have received a copy of the GNU General Public License
+	 * along with Foobar.  If not, see <http://www.gnu.org/licenses/>
+	 */
+	
+
 	class YADC_Model
 	{
 		const LockMetaName = '_yadc_update_lock';
